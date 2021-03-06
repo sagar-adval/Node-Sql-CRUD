d07521a5ab580edb733ec898f3627958b04edbef
Sagar Adval <sagaradval@gmail.com>
2021-03-06 09:11:22 +0530

    Connection to db and create operations done

---
+++ app/controllers/tutorial.controller.js
@@ -0,0 +1,25 @@
+const db = require('../models');
+
+const Tutorial = db.tutorials;
+
+const Op = db.Sequelize.Op;
+
+exports.create = async (req, res)  => {
+    if (!req.body.title) {
+        return res.status(400).send({
+          message: "Title can not be empty!"
+        });
+      }
+
+      const tutorial = req.body;
+
+      const createdTut = await Tutorial.create(tutorial);
+
+      if(createdTut)
+      {
+        return res.status(200).send(createdTut)
+      }
+
+      return res.send(500).send('Could not create the tutorial');
+}
+
--- server.js
+++ server.js
@@ -7,12 +7,21 @@ dotenv.config();
 app.use(express.json())
 
 const db = require('./app/models/index')
-db.sequelize.sync();
+// db.sequelize.sync();
 // console.log(db);
+
+
+db.sequelize.sync({ force: true }).then(() => {
+  console.log("Drop and re-sync db.");
+});
 
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to bezkoder application." });
   });
+
+  const tutorialController = require("./app/routes/tutorial.routes");
+
+  app.use("/api/tutorials", tutorialController);
   
   // set port, listen for requests
   const PORT = process.env.PORT || 8080;
